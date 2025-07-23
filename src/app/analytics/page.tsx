7a1e81e7ca74c47d63ebf76d03dba20fe8068f51
Utkarsh Dwivedi <utkarshdwivedi1709@gmail.com>
2025-07-24 02:02:10 +0530

    fda

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -809,16 +809,6 @@ export default function AnalyticsPage() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 to-slate-800">
-      {/* Development Notice Popup */}
-      <div className="bg-yellow-500 text-black px-4 py-3 text-center font-medium shadow-lg">
-        <div className="flex items-center justify-center space-x-2">
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-          </svg>
-          <span>Feature still Under development</span>
-        </div>
-      </div>
-
       <div className="container mx-auto p-6">
         <div className="max-w-7xl mx-auto space-y-6">
           {/* Header */}
